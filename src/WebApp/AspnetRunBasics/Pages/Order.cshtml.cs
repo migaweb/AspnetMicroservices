fc0bfd8ad63c21087a634bab33396b4ff72737a2
Mikael Storman Gustavsson <miga02@gmail.com>
2021-05-17 19:49:56 +0200

    Added and refactored web client

---
+++ src/WebApp/AspnetRunBasics/Pages/Order.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics
+{
+  public class OrderModel : PageModel
+  {
+    private readonly IOrderService _orderService;
+
+    public OrderModel(IOrderService orderService)
+    {
+      _orderService = orderService;
+    }
+
+    public IEnumerable<OrderResponseModel> Orders { get; set; } = new List<OrderResponseModel>();
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+      var userName = "swn";
+      Orders = await _orderService.GetOrdersByUserName(userName);
+
+      return Page();
+    }
+  }
+}
