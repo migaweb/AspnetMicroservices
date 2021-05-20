3cb846f97db015a3926c3de6f91426d67cffdc34
Mikael Storman Gustavsson <miga02@gmail.com>
2021-05-20 19:46:38 +0200

    Added seri logger and logging delegating handler.

--- src/WebApp/AspnetRunBasics/Startup.cs
+++ src/WebApp/AspnetRunBasics/Startup.cs
@@ -1,5 +1,6 @@
 
 using AspnetRunBasics.Services;
+using Common.Logging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,21 +22,23 @@ namespace AspnetRunBasics
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddTransient<LoggingDelegatingHandler>();
+
       services.AddHttpClient<ICatalogService, CatalogService>(c =>
-                c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]));
-                //.AddHttpMessageHandler<LoggingDelegatingHandler>()
+                c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>();
                 //.AddPolicyHandler(GetRetryPolicy())
                 //.AddPolicyHandler(GetCircuitBreakerPolicy());
 
       services.AddHttpClient<IBasketService, BasketService>(c =>
-          c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]));
-          //.AddHttpMessageHandler<LoggingDelegatingHandler>()
+          c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]))
+          .AddHttpMessageHandler<LoggingDelegatingHandler>();
           //.AddPolicyHandler(GetRetryPolicy())
           //.AddPolicyHandler(GetCircuitBreakerPolicy());
 
       services.AddHttpClient<IOrderService, OrderService>(c =>
-          c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]));
-          //.AddHttpMessageHandler<LoggingDelegatingHandler>()
+          c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]))
+          .AddHttpMessageHandler<LoggingDelegatingHandler>();
           //.AddPolicyHandler(GetRetryPolicy())
           //.AddPolicyHandler(GetCircuitBreakerPolicy());
 
