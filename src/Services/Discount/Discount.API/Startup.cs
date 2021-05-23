002cb012d0e9507a2ddb0a4eaf57cf1cadd3f1ab
Mikael Storman Gustavsson <miga02@gmail.com>
2021-05-23 12:40:51 +0200

    Added health check for the discount API

--- src/Services/Discount/Discount.API/Startup.cs
+++ src/Services/Discount/Discount.API/Startup.cs
@@ -1,9 +1,12 @@
 using Discount.API.Repositories;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
@@ -32,6 +35,9 @@ namespace Discount.API
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
       });
+
+      services.AddHealthChecks()
+        .AddNpgSql(Configuration["DatabaseSettings:ConnectionString"]);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +57,11 @@ namespace Discount.API
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
+        endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+        {
+          Predicate = _ => true,
+          ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
       });
     }
   }
